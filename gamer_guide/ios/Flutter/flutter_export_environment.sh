3271b31a5d4ad46db1f87a0d00043c72b942954f
BoDZz98 <abdelrahman1904368@miuegypt.edu.eg>
2022-12-09 20:35:01 +0200

    s

--- gamer_guide/ios/Flutter/flutter_export_environment.sh
+++ gamer_guide/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\Users\boudy\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\boudy\OneDrive\Desktop\Flutter Projects\mobile_app_final"
+export "FLUTTER_APPLICATION_PATH=C:\Users\boudy\OneDrive\Desktop\gamers_guide\gamer_guide"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
