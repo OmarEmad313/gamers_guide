0675384d90597bbdd22cb2dfe80a5238f3317c61
BoDZz98 <abdelrahman1904368@miuegypt.edu.eg>
2022-12-16 20:03:46 +0200

    s

--- gamer_guide/.dart_tool/dartpad/web_plugin_registrant.dart
+++ gamer_guide/.dart_tool/dartpad/web_plugin_registrant.dart
@@ -6,12 +6,18 @@
 // @dart = 2.13
 // ignore_for_file: type=lint
 
+import 'package:cloud_firestore_web/cloud_firestore_web.dart';
+import 'package:firebase_auth_web/firebase_auth_web.dart';
+import 'package:firebase_core_web/firebase_core_web.dart';
 import 'package:video_player_web/video_player_web.dart';
 import 'package:wakelock_web/wakelock_web.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 void registerPlugins([final Registrar? pluginRegistrar]) {
   final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
+  FirebaseFirestoreWeb.registerWith(registrar);
+  FirebaseAuthWeb.registerWith(registrar);
+  FirebaseCoreWeb.registerWith(registrar);
   VideoPlayerPlugin.registerWith(registrar);
   WakelockWeb.registerWith(registrar);
   registrar.registerMessageHandler();
